d713ad67c1e655c311c83f7e1376fe1a09a1fa94
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-02-20 23:42:42 -0500

    Python3 Solution
            Submission Statistics:
            Question #: 268
            Language: Python3
            Runtime: 1473 ms
            Runtime percentile: 8.59
            Memory: 17.756 MB
            Memory percentile: 87.83

---
+++ 268. Missing Number/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        x = len(nums)
+        for i in range(x+1):
+            if i not in nums:
+                return i
+        return -1
+
+        
