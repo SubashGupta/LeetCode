d74f680899d0112b311a7e1164206ca3ca028e33
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-02-22 21:20:56 -0500

    Python3 Solution
            Submission Statistics:
            Question #: 187
            Language: Python3
            Runtime: 7700 ms
            Runtime percentile: 5.02
            Memory: 17.028 MB
            Memory percentile: 99.25

--- 187. Repeated DNA Sequences/Solution.py
+++ 187. Repeated DNA Sequences/Solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        lst=[]
+        lst=set()
         for i in range(len(s)-10):
             x=s[i:i+10]
-            if x in s[i+1:]:
-                lst.append(x)
-        return list(set(lst))   
+            if x in s[i+1:] and x not in lst:
+                lst.add(x)
+        return list(lst)  
