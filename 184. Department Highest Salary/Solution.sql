2bcec2ed39afc14331b9209c4e47b2cf734f526f
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-02-22 21:07:29 -0500

    MySQL Solution
            Submission Statistics:
            Question #: 184
            Language: MySQL
            Runtime: 1097 ms
            Runtime percentile: 81.44
            Memory: 0 MB
            Memory percentile: 100.00

---
+++ 184. Department Highest Salary/Solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select d.name as Department, e.name as Employee, e.salary as Salary
+from Employee e ,Department d where e.departmentId = d.id 
+and (e.departmentId,e.salary) in 
+    (select departmentId, max(salary) 
+    from employee 
+    group by departmentId
+    );
