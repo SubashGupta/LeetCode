1cb3a952857351da5bb5e54366bbd41c52e213e2
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-03-26 21:49:46 -0400

    Time: 518 ms (27.33%), Space: 19.1 MB (78.54%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -1,28 +1,24 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        i = 0
-        j = 0
-        c = 0
+        i=0
+        j=0
+        c=0
         prod = 1
-        caches = True
-        
-        if k <= 1:
+        caches  = True
+        if k <=1:
             return 0
-        
-        while j < len(nums):
+        while j<len(nums):
             if caches:
                 prod *= nums[j]
-                
-            if prod < k:
-                c += j - i + 1
-                j += 1
-                # print(c, i, j)
+            if prod<k:
+                c+=j-i+1
+                j+=1
+                #print(c,i,j)
                 caches = True
             else:
-                prod //= nums[i]
-                i += 1
+                prod//=nums[i]
+                i+=1
                 caches = False
-                
         return c
                 
         
