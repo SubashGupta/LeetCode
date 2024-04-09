9da8889e99058f3fe4fdda56f810e46341363cf6
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-04-08 20:47:58 -0400

    Time: 28 ms (99.21%), Space: 16.7 MB (5.73%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,7 +1,12 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
         time=0
-        
+        for i in range(len(tickets)):
+            if i<=k:
+                time+=min(tickets[i],tickets[k])
+            else:
+                time+=min(tickets[i],tickets[k]-1)
+        '''
         while tickets[k] !=0:
             i=0
             while i<len(tickets):   
@@ -13,4 +18,5 @@ class Solution:
                 if tickets[k] == 0:
                     break
                 i+=1
+            '''
         return time
