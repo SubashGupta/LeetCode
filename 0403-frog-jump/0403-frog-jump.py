cf8a86d123c6fe4c26b1fa84ea2d9f68ef4cd384
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-04-10 21:29:30 -0400

    Time: 191 ms (47.18%), Space: 18.3 MB (88.28%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -1,16 +1,12 @@
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
-        # Create a dictionary to store the jump distances that can reach each stone
-        dp = {stone: set() for stone in stones}
-        dp[0] = {0}  # The frog starts at stone 0 with a jump distance of 0
-        
+        dp={}
+        for stone in stones:
+              dp[stone] = set()
+        dp[0] = {0}
         for stone in stones:
             for jump in dp[stone]:
-                # Try all possible next jumps: k-1, k, k+1
                 for next_jump in range(jump - 1, jump + 2):
                     if next_jump > 0 and stone + next_jump in dp:
                         dp[stone + next_jump].add(next_jump)
-        
-        # If the set of jump distances for the last stone is not empty, return True
         return bool(dp[stones[-1]])
-                
