1686df06d2ac1e32c1078b12a29afa7530edc5cc
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-04-18 00:36:14 -0400

    Time: 277 ms (79.25%), Space: 32 MB (71.01%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,11 +4,38 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        x = head
-        op = ""
-        while x:
-            op+=str(x.val)
-            x=x.next
-        return op == op[::-1]
+    def reversel(self, head):
+        prev = None
+        start = head
+        while start:
+            val = start.next
+            start.next = prev
+            prev = start
+            start = val
+        return prev
+    
+    def isPalindrome(self, head):
+        if not head or not head.next:
+            return True
         
+        # Find the middle of the linked list
+        slow = head
+        fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Reverse the second half of the linked list
+        second_half_reversed = self.reversel(slow.next)
+        
+        # Separate the first and second halves of the linked list
+        slow.next = None
+        
+        # Compare values of nodes in the first and reversed second half
+        while head and second_half_reversed:
+            if head.val != second_half_reversed.val:
+                return False
+            head = head.next
+            second_half_reversed = second_half_reversed.next
+        
+        return True
