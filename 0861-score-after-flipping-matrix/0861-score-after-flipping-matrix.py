c7342cbf22f8822dd74a382e9d2d374ffb689ffc
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-05-13 00:25:20 -0400

    Time: 28 ms (99.14%), Space: 16.4 MB (92.67%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -0,0 +1,27 @@
+class Solution:
+    def matrixScore(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        columns = len(grid[0])
+        for i in range(rows):
+            if grid[i][0] == 0:
+                #logic to flip
+                for j in range(columns):
+                    grid[i][j] = 1 - grid[i][j]
+        
+        for j in range(1, columns):
+            #counting the number of zeros in each column
+            c=0
+            for i in range(rows):
+                if grid[i][j] == 0:
+                    c+=1
+            if c > rows-c:
+                for k in range(rows):
+                    grid[k][j] ^= 1
+        
+        sums = 0
+        for i in range(rows):
+            x=''
+            for j in range(columns):
+                x+=str(grid[i][j])
+            sums+=int(x,2)
+        return sums
