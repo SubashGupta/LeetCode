e936ed72a67c3334e47d6e772e57da97be563bce
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-05-12 23:19:09 -0400

    Time: 133 ms (47.79%), Space: 17 MB (97.72%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
@@ -1,19 +1,18 @@
 class Solution:
-    def find_max(self, grid, x, y):
-        max_element = 0
-        for i in range(x, x + 3):
-            for j in range(y, y + 3):
-                max_element = max(max_element, grid[i][j])
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        final = []
         
-        return max_element
-
-    def largestLocal(self, grid):
-        N = len(grid)
-        
-        max_local = [[0] * (N - 2) for _ in range(N - 2)]
-        for i in range(N - 2):
-            for j in range(N - 2):
-                max_local[i][j] = self.find_max(grid, i, j)
-        
-        return max_local
-                
+        size = len(grid)
+        for i in range(size-2):
+            maxi=[]
+            for j in range(size-2):
+                maxi.append(self.maxofgrid(grid, i, j))
+            final.append(maxi)
+        return final
+    
+    def maxofgrid(self, grid, x,y):
+        maxi = 0
+        for i in range(x,x+3):
+            for j in range(y,y+3):
+                maxi = max(maxi, grid[i][j])
+        return maxi
