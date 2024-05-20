c998c06db6faaec9f5792fb822c00771df1d0df9
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-05-19 23:15:35 -0400

    Time: 101 ms (43.88%), Space: 18 MB (25.80%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        conditions = n/3
+        if n == 1 or n == 2:
+            return list(set(nums))
+        op=[]
+        nums.sort()
+        c=1
+        maxval = nums[0]
+        maxvalcount = 1
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                c+=1
+            else:
+                if c>conditions:
+                    c = 1
+                    op.append(nums[i-1])
+                else:
+                    c = 1
+        if c>conditions:
+            op.append(nums[-1])
+        return op
