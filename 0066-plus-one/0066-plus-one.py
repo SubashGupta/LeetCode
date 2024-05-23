0fa40a5fdf17f3147a417bc1c88a4c802ddc81de
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-05-22 20:06:26 -0400

    Time: 39 ms (39.68%), Space: 16.6 MB (10.59%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,16 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        total=0
+        size=len(digits)
+        for i in digits:
+            total=total*10+i
+        total+=1
+        if len(str(total)) > size:
+            digits.append(total%10)
+            total=total//10
+        for i in range(size-1,-1,-1):
+            print("alsocamein")
+            digits[i] = total%10
+            total=total//10
+        return digits
+        
