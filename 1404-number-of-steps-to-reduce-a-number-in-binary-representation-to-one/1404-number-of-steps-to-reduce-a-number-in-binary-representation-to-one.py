0c573cbc9b473920a766addde0cfdbc93953ae20
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-05-28 23:32:44 -0400

    Time: 34 ms (75.19%), Space: 16.5 MB (94.14%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -1,5 +1,6 @@
 class Solution:
     def numSteps(self, s: str) -> int:
+        '''
         binval = int(s,2)
         c=0
         while binval != 1:
@@ -9,3 +10,22 @@ class Solution:
                 binval//=2
             c+=1
         return c
+        '''
+        count=0
+        carry=0
+        l=len(s)-1
+        while l>0:
+            if int(s[l])+carry == 0:
+                carry = 0
+                count+=1
+            elif int(s[l])+carry == 2:
+                count+=1
+                carry = 1
+            else:
+                count+=2
+                carry=1
+            l-=1
+        if carry ==1:
+            count+=1
+        return count
+                
