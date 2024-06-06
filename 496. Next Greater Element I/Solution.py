b29d18a5351e6add56e0389cd0d6d8404b60963b
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-06-05 20:08:14 -0400

    Python3 Solution
            Submission Statistics:
            Question #: 496
            Language: Python3
            Runtime: 45 ms
            Runtime percentile: 76.64
            Memory: 16.732 MB
            Memory percentile: 81.16

---
+++ 496. Next Greater Element I/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        op=[]
+        d={}
+        for i in range(len(nums2)):
+            d[nums2[i]] = i
+        for j in nums1:
+            indexStart = d[j] + 1
+            maxi=-1
+            while indexStart<len(nums2):
+                if nums2[indexStart]>j:
+                    maxi = nums2[indexStart]
+                    break
+                indexStart+=1
+            op.append(maxi)
+        return op
+        
