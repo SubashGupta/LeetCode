565791d9598581bd6e00ceffc7f7b4c258af841e
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-06-11 19:04:05 -0400

    Python3 Solution
            Submission Statistics:
            Question #: 203
            Language: Python3
            Runtime: 51 ms
            Runtime percentile: 43.06
            Memory: 19.448 MB
            Memory percentile: 45.42

---
+++ 203. Remove Linked List Elements/Solution.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], vali: int) -> Optional[ListNode]:
+        if not head:
+            return None
+        while head and head.val == vali:
+            head = head.next
+        curr = head
+        while curr and curr.next:
+            if curr.next.val == vali:
+                curr.next  = curr.next.next
+            else:
+                curr = curr.next
+        return head
+        
