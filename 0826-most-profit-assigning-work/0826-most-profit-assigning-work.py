ba4bbff5e9e92e449580a307f1557ecd519437d8
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-06-17 21:47:37 -0400

    Time: 268 ms (75.53%), Space: 19.3 MB (93.65%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -2,31 +2,22 @@ class Solution:
     def maxProfitAssignment(
         self, difficulty: List[int], profit: List[int], worker: List[int]
     ) -> int:
-        job_profile = [(0, 0)]
-        for i in range(len(difficulty)):
-            job_profile.append((profit[i], difficulty[i]))
+        job_profile = [
+            (difficulty[i], profit[i]) for i in range(len(difficulty))
+        ]
 
-        # Sort in decreasing order of profit.
-        job_profile.sort(reverse=True)
-        for i in range(len(job_profile) - 1):
-            job_profile[i + 1] = (
-                job_profile[i + 1][0],
-                min(job_profile[i][1], job_profile[i + 1][1]),
-            )
+        # Sort both worker and job_profile arrays
 
-        net_profit = 0
+        worker.sort()
+        job_profile.sort()
+
+        net_profit, max_profit, index = 0, 0, 0
         for ability in worker:
-            # Maximize profit using binary search.
-            l, r = 0, len(job_profile) - 1
-            job_profit = 0
-            while l <= r:
-                mid = (l + r) // 2
-                if job_profile[mid][1] <= ability:
-                    job_profit = max(job_profit, job_profile[mid][0])
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            # Add profit of each worker to total profit.
-            net_profit += job_profit
+            # While the index has not reached the end and worker can pick a job
+            # with greater difficulty move ahead.
 
+            while index < len(difficulty) and ability >= job_profile[index][0]:
+                max_profit = max(max_profit, job_profile[index][1])
+                index += 1
+            net_profit += max_profit
         return net_profit
