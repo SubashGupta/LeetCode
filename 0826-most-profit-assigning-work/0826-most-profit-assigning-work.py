b33896cb11641af267179f47c6097464b2fe9619
Subash Gupta Karamsetty <69724057+SubashGupta@users.noreply.github.com>
2024-06-17 21:57:47 -0400

    Time: 274 ms (60.90%), Space: 19.9 MB (19.78%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -1,23 +1,15 @@
 class Solution:
-    def maxProfitAssignment(
-        self, difficulty: List[int], profit: List[int], worker: List[int]
-    ) -> int:
-        job_profile = [
-            (difficulty[i], profit[i]) for i in range(len(difficulty))
-        ]
-
-        # Sort both worker and job_profile arrays
-
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        combined = list(zip(difficulty, profit))
+        combined.sort(key=lambda x: x[0])
+        difficulty, profit = zip(*combined)
         worker.sort()
-        job_profile.sort()
-
-        net_profit, max_profit, index = 0, 0, 0
-        for ability in worker:
-            # While the index has not reached the end and worker can pick a job
-            # with greater difficulty move ahead.
-
-            while index < len(difficulty) and ability >= job_profile[index][0]:
-                max_profit = max(max_profit, job_profile[index][1])
-                index += 1
-            net_profit += max_profit
-        return net_profit
+        total=0
+        j=0
+        profits=0
+        for i in worker:
+            while j < len(difficulty) and difficulty[j]<=i:
+                    profits = max(profits, profit[j])
+                    j+=1
+            total+=profits
+        return total
